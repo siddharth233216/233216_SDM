c1eae40cb29cfde697118e1e5d43c4e6d0b134e4
dac <dac@dypm.org>
2023-05-17 12:44:13 +0530

    examSdm

---
+++ db.sql
@@ -0,0 +1,16 @@
+create table job (
+category varchar(20),
+cv id int primary key,
+options int,
+requirement (20)
+
+);
+
+create table your profile(
+name v,
+qualifilication varchar(20),
+category varchar(20),
+cv id int,
+foreign key f1 (cvid) references job (cvid)
+
+);
